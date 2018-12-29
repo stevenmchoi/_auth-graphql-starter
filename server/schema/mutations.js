c53ba94f38ae6b2b02db64b8a855efecf190f3fb
Steven Choi <steven.m.choi@gmail.com>
2018-12-29 00:48:33 -0800

    Add logout mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -17,6 +17,14 @@ const mutation = new GraphQLObjectType({
 				return AuthService.signup({ email, password, req });
 			},
 		},
+		logout: {
+			type: UserType,
+			resolve(parentValue, args, req) {
+				const { user } = req;
+				req.logout();
+				return user;
+			},
+		},
 	},
 });
 
