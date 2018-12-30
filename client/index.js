86a9721612aa296ac77ee22a89c1869f648c9781
Steven Choi <steven.m.choi@gmail.com>
2018-12-30 00:07:06 -0800

    Set up Router and Route to App component

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 const client = new ApolloClient({
 	dataIdFromObject: (o) => o.id,
@@ -10,7 +11,9 @@ const client = new ApolloClient({
 const Root = () => {
 	return (
 		<ApolloProvider client={client}>
-			<div>Auth Starter</div>
+			<Router history={hashHistory}>
+				<Route path="/" component={App} />
+			</Router>
 		</ApolloProvider>
 	);
 };
