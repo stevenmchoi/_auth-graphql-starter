670c25834d1d870805ab905ee4ef38f4c6e693ac
Steven Choi <steven.m.choi@gmail.com>
2018-12-30 23:47:24 -0800

    Query for userId

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 class Header extends Component {
 	render() {
+		console.log('this.props.data', this.props.data);
+
+		if (this.props.data.loading) {
+			return <div>Loading...</div>;
+		}
+
 		return <div>Header</div>;
 	}
 }
 
-export default Header;
+export default graphql(query)(Header);
